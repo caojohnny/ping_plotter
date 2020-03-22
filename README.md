af3ddf0c7df7d8b4f13511038c54131be8b73ccb
AgentTroll <5037004+AgentTroll@users.noreply.github.com>
2020-03-22 03:58:41 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# `ping_plotter`
+
+I've been having consistent ping issues for a bit now for
+some reason and I thought it might be useful to write a
+little script to help me visualize/quantify those issues.
+
+The purpose of this tool is simply to ping a single server
+for however many times and then plot 3 metrics: the
+round-trip time (RTT), the average RTT and finally the
+numeber of ping packets lost.
+
+# Implementation
+
+This utilizes Python's `subprocess` module to run the
+`ping` command several times. The results are then plotted
+in real time using `matplotlib`.
+
+# Usage
+
+``` shell
+git clone https://github.com/AgentTroll/ping_plotter.git
+cd ping_plotter
+python3 ping_plotter.py [ping_target]
+```
+
+Requires matplotlib (`pip3 install matplotlib`) and some
+compatible GUI toolkit like python-tk (`apt-get install
+python3-tk`) installed.
+
+# Demo
+
+![My RTT Ping to Github.com Over 30 Minutes](https://i.postimg.cc/tXdznDCk/11027188-36-Ping.png)
+
+# Caveats
+
+  * Unix (macOS/Linux) only - uses the `ping` command
+
+# Credits
+
+Built with [neovim](https://neovim.io/)
+
+Uses [Matplotlib](https://matplotlib.org/)
+
---
+++ ping_plotter.py
@@ -0,0 +1,106 @@
+import matplotlib.pyplot as plt
+import subprocess as sp
+import sys
+
+def get_last(x):
+    x_list = list(x)
+    return 0 if not x_list else x_list[-1]
+
+if len(sys.argv) != 2:
+    print("Ping target required")
+    sys.exit(0)
+
+ping_target = sys.argv[1]
+print("ping_target = " + ping_target)
+
+ping_cmd = [ "ping", "-c", "1", ping_target ]
+
+plot_length = 30 * 60
+plot_ping_fmt = "k-*"
+plot_mean_fmt = "r:+"
+plot_dropped_fmt = "b--x"
+plot_ping_label = "RTT Ping"
+plot_mean_label = "RTT Ping Moving Average"
+plot_dropped_label = "Dropped packets"
+plot_title = "My RTT Ping to " + ping_target + " Over 30 Minutes"
+plot_xlabel = "Time (s)"
+plot_ylabel = "RTT Ping (ms)"
+
+# X and Y axis data
+iterations = []
+ping_times = []
+
+# Moving average
+means = []
+
+# Dropped packet count
+dropped = []
+
+for i in range(0, plot_length):
+    iterations.append(i)
+
+    # Run the ping command
+    run_result = sp.run(ping_cmd, stdout = sp.PIPE)
+
+    # Process the ping output
+    output_str = str(run_result.stdout)
+    output_arr = output_str.split("\\n")
+    comp = output_arr[1]
+
+    # Dropped packet - no response string to parse
+    if comp == "":
+        print("Packet dropped")
+
+        # Update data to reflect dropped packet
+        # ST for ping and +1 dropped
+        prev_ping = get_last(ping_times)
+        prev_mean = get_last(means)
+        prev_dropped = get_last(dropped)
+
+        ping_times.append(prev_ping)
+        means.append(prev_mean)
+        dropped.append(prev_dropped + 1)
+        continue
+
+    # Print component string for debugging purposes
+    print(comp)
+
+    time_eq_idx = comp.rfind("=")
+    time_space_idx = comp.rfind(" ")
+    time_str = comp[time_eq_idx + 1:time_space_idx]
+
+    # Select the time and convert to a number
+    time = float(time_str)
+
+    # Add to the axis data
+    ping_times.append(time)
+
+    # Compute mean
+    n = len(means)
+    prev_mean = get_last(means)
+    new_mean = (prev_mean * n + time) / (n + 1)
+
+    prev_dropped = get_last(dropped)
+    dropped.append(prev_dropped)
+
+    # Add mean to axis
+    means.append(new_mean)
+
+    # Plot asthetics
+    plt.clf()
+    plt.ion()
+    plt.title(plot_title)
+    plt.xlabel(plot_xlabel)
+    plt.ylabel(plot_ylabel)
+    plt.grid()
+
+    # Plot the data
+    plt.plot(iterations, ping_times, plot_ping_fmt, label = plot_ping_label)
+    plt.plot(iterations, means, plot_mean_fmt, label = plot_mean_label)
+    plt.plot(iterations, dropped, plot_dropped_fmt, label = plot_dropped_label)
+    plt.legend(loc = "upper right")
+    plt.draw()
+    plt.pause(1)
+
+plt.pause(sys.maxsize)
+
